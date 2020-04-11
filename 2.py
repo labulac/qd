4139a5e8694358bfdc660d37bf31760219e370d8
labulac <35835318+labulac@users.noreply.github.com>
2020-04-11 11:36:55 +0800

    1

--- 2.py
+++ 2.py
@@ -10,16 +10,13 @@ b = r.text
 print(b)
 
 if a != b:
+    print("检测到更新")
     k = requests.get("https://qd.labulac.top/1.py")
     with open("1.py", 'w') as f:
         f.write(k.text)
-        print(k.text)
-    j = requests.get("https://qd.labulac.top/1.sh")
-    with open("1.sh", 'w') as f:
-        f.write(j.text)
-        print(j.text)
     with open("1.txt", 'w') as f:
         f.write(b)
+    print("已更新完成")
 
 else:
     print("无更新")
