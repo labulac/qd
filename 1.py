ccffb6d129b26928669fffd8f30addab460c8804
labulac <35835318+labulac@users.noreply.github.com>
2020-04-11 12:30:23 +0800

    1

--- 1.py
+++ 1.py
@@ -46,7 +46,7 @@ def push(name):
 update()
 option = webdriver.ChromeOptions()
 option.add_argument('--no-sandbox')
-#option.add_argument('--headless')
+option.add_argument('--headless')
 option.add_argument('--incognito')
 option.add_argument('--blink-settings=imagesEnabled=false')
 option.add_argument('--start-maximized')
