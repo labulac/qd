dfd48b87c8db58fa35a64bcb9733061293a67b61
labulac <35835318+labulac@users.noreply.github.com>
2020-04-12 13:51:06 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -254,7 +254,7 @@ def wuai(n):
             print("???")
             push(n)
         else:
-            print(n+"ok11112")
+            print(n+"ok2222")
     except Exception as e:
         print(e)
 
