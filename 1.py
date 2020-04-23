613d89817002f9970d481d861c8a7f5ea764661b
labulac <35835318+labulac@users.noreply.github.com>
2020-04-23 12:42:01 +0800

    1

--- 1.py
+++ 1.py
@@ -236,12 +236,10 @@ def pcbetanew(u, n):
         "Cookie": cookie,
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Mobile/15E148 Safari/604.1"}
 
-
     try:
 
-
         html = requests.get(url1, headers=headers).text
-        #print(html)
+        # print(html)
 
         time.sleep(5)
 
