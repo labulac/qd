84175a0c688ab38fe981695071a76563615ed19a
labulac <35835318+labulac@users.noreply.github.com>
2021-01-05 23:32:35 +0800

    Update create_sheet_info.js

--- create_sheet_info.js
+++ create_sheet_info.js
@@ -3,8 +3,8 @@ yuan=C:/Users/muban.xlsx
 xian=D:/
 
 [update]
-newversion=16
-downloadurl=https://cdn.jsdelivr.net/gh/labulac/qd@master/16.exe
+newversion=17
+downloadurl=https://cdn.jsdelivr.net/gh/labulac/qd@master/17.
 
 [yiyan]
 yiyan=https://v1.jinrishici.com/rensheng.txt
